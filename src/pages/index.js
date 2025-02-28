3adbf69623900e7dbeed48570f0a5a012ae8d9a3
juihung_kao <rayhungkao@gmail.com>
2025-02-28 15:59:41 +0800

    [what] add strapi for CMS - use dummy data for dev/test use

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,25 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { useEffect, useState } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// Define Strapi URL
+const STRAPI_URL = "http://localhost:1337";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export default function Home(props) {
+  const [articles, setArticles] = useState([]);
 
-export default function Home() {
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+
+  const getArticles = async () => {
+    const response = await fetch(`${STRAPI_URL}/api/articles?populate=*`);
+    const data = await response.json();
+    console.log(data.data)
+    setArticles(data.data);
+  };
+
   return (
     <>
       <Head>
@@ -22,94 +28,57 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
-        <main className={styles.main}>
+      <div>
+        <main>
           <Image
-            className={styles.logo}
-            src="/next.svg"
+            src="/assets/images/logo/duce-logo.svg"
             alt="Next.js logo"
             width={180}
-            height={38}
+            height={100}
             priority
           />
-          <ol>
-            <li>
-              Get started by editing <code>src/pages/index.js</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
-            >
-              Read our docs
-            </a>
+          <div>
+            <h1>Blog posts from Strapi</h1>
+            <div>
+              <h2>Articles</h2>
+              <div>
+                {articles.map((article) => (
+                  <article
+                    key={article.id}
+                  >
+                    {Array.isArray(article.cover) ? (
+                      article.cover.map((cover, index) => (
+                        <Image
+                          key={index}
+                          src={STRAPI_URL + article.cover[index].url}
+                          alt={`Article ${index + 1} Cover`}
+                          width={180}
+                          height={38}
+                          priority
+                        />
+                      ))
+                    ) : (
+                      <div>
+                        <p>No cover image available</p>
+                      </div>
+                    )}
+                    <div>
+                      <h3>
+                        {article.title}
+                      </h3>
+                      <p>{article.content}</p>
+                      <p>
+                        Published: {article.publishedAt}
+                      </p>
+                    </div>
+                  </article>
+                ))}
+              </div>
+            </div>
           </div>
         </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
+        <footer>
+          ©2025 Duce. All rights reserved.
         </footer>
       </div>
     </>
